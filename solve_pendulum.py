8452ab5f235ae183cab4d8ef9fa37c594b3ba826
logic-three-body <1552451580@qq.com>
2021-02-20 12:27:02 +0800

    Open a window change its background color

--- solve_pendulum.py
+++ solve_pendulum.py
@@ -2,7 +2,7 @@ import taichi as ti
 ti.init(arch=ti.cpu)
 
 def main():
-    gui=ti.GUI('Pendulum System')
+    gui=ti.GUI('Pendulum System',background_color=0xfaf3e0)
     while True:
         gui.show()
 
