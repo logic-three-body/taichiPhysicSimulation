7a6c02670e22780ce87d13eedb7ed877aca19578
logic-three-body <1552451580@qq.com>
2021-02-21 20:02:39 +0800

    clean the code

--- pendulum-test.py
+++ pendulum-test.py
@@ -6,7 +6,7 @@ g = 9.8
 L = 0.5 #单摆长 要更改
 mu = 0.1
 
-THETA_0 = np.pi / 3  # 60 degrees
+THETA_0 = np.pi / 0.3  # 30 degrees
 THETA_DOT_0 = 0  # No initial angular velocity
 ti.init(arch=ti.cpu)
 x=ti.Vector.field(2,dtype=ti.f32,shape=max_num_dots)#位置
@@ -43,19 +43,14 @@ def main():
     gui=ti.GUI('pendulum test',background_color=0xfaf3e0)
 
 
-    while True:
-        for e in gui.get_events(ti.GUI.PRESS):
-            if e.key==ti.GUI.LMB:
-                print(e.pos)
 
-        delta_t = 0.01  # Some time step
-        t=100
-        theta = THETA_0
-        theta_dot = THETA_DOT_0
-        x[0]=ti.Vector([0.5,1])
-        #x[1]=ti.Vector([0.5,0.5])
-        for time in np.arange(0, t, delta_t):
-            x[1]=ti.Vector([x[0][0]-L*ti.sin(theta),x[0][1]-L*ti.cos(theta)])
+    delta_t = 0.01  # Some time step
+    t=1000#时间范围
+    theta = THETA_0#初始化变化角
+    theta_dot = THETA_DOT_0
+    x[0]=ti.Vector([0.5,0.9])#悬挂点
+    for time in np.arange(0, t, delta_t):#core
+            x[1]=ti.Vector([x[0][0]-L*ti.sin(theta),x[0][1]-L*ti.cos(theta)])#摆动点
             #x[1]=ti.Vector([x[0][0]+0.1,x[0][1]-0.6])
             print(x)
             print(x[1][0],x[1][1])
@@ -63,7 +58,7 @@ def main():
             gui.circle(x[1],color=0xb68973,radius=10)
             gui.line(begin=x[0], end=x[1], color=0xff75a0, radius=2)
             gui.show()
-            theta_double_dot = get_theta_double_dot(
+            theta_double_dot = get_theta_double_dot(#update
                 theta, theta_dot
             )
             theta += theta_dot * delta_t
